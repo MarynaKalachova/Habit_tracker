bdf0d406ac411f5d6bcbaf332d0ab2d98787550d
Maryna Kalachova <kalachova.maryna@gmail.com>
2016-07-03 14:18:15 -0400

    Create HabitTrackerDBHelper class

--- app/src/main/java/com/example/android/habittracker/db/HabitContract.java
+++ app/src/main/java/com/example/android/habittracker/db/HabitContract.java
@@ -4,7 +4,6 @@ package com.example.android.habittracker.db;
 import android.provider.BaseColumns;
 
 public class HabitContract {
-    public static final String DB_NAME = "com.example.android.habittracker.db";
 
     public HabitContract(){}
 
@@ -14,12 +13,6 @@ public class HabitContract {
         public static final String COLUMN_NAME_HABIT_NAME = "habit_name";
         public static final String COLUMN_NAME_DURATION = "duration";
     }
-
-    private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + HabitEntry.TABLE_NAME + " (" +
-                    HabitEntry.COLUMN_NAME_HABIT_ID + " INTEGER PRIMARY KEY," +
-                    HabitEntry.COLUMN_NAME_HABIT_NAME + " TEXT" + ", " +
-                    HabitEntry.COLUMN_NAME_DURATION + " INTEGER" + " )";
 
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + HabitEntry.TABLE_NAME;
---
+++ app/src/main/java/com/example/android/habittracker/db/HabitTrackerDBHelper.java
@@ -0,0 +1,36 @@
+package com.example.android.habittracker.db;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class HabitTrackerDBHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "HabitTracker.db";
+
+    public HabitTrackerDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_ENTRIES =
+                "CREATE TABLE " + HabitContract.HabitEntry.TABLE_NAME + " (" +
+                        HabitContract.HabitEntry.COLUMN_NAME_HABIT_ID + " INTEGER PRIMARY KEY," +
+                        HabitContract.HabitEntry.COLUMN_NAME_HABIT_NAME + " TEXT" + ", " +
+                        HabitContract.HabitEntry.COLUMN_NAME_DURATION + " INTEGER" + " )";
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + HabitContract.HabitEntry.TABLE_NAME;
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+
+    }
+}
